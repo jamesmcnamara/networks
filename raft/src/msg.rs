0e5d5dde0a0bfb381ada938e6bdaf6811ba7264b
jamesmcnamara <jamesscottmcnamara@gmail.com>
2015-11-30 18:48:54 -0500

    added deserialization for messages

--- raft/src/msg.rs
+++ raft/src/msg.rs
@@ -1,19 +1,26 @@
 use std::collections::BTreeMap;
 
-use rustc_serialize::json::{Json, ToJson};
+use rustc_serialize::json::{Json, Object, ToJson};
 
 use super::node::NodeId;
 
+macro_rules! get {
+    ($obj:ident -> $key:expr; $parser:path) => {{
+        let err = format!("Json parse failed for {}", $key);
+        $parser($obj.get($key).expect(&err)).expect(&err)
+    }}
+}
 pub trait AddJson {
     fn add_json<T: ToJson>(&mut self, key: &'static str, val: T);
 }
 
-impl AddJson for BTreeMap<String, Json> {
+impl AddJson for Object {
     fn add_json<T: ToJson>(&mut self, key: &'static str, val: T) {
         self.insert(key.to_owned(), val.to_json());
     }
 }
 
+#[derive(PartialEq, Debug)]
 struct BaseMsg {
     src: NodeId,
     dst: NodeId,
@@ -22,28 +29,82 @@ struct BaseMsg {
 }
 
 impl BaseMsg {
-    fn fill(&self, d: &mut BTreeMap<String, Json>) {
+    fn fill(&self, d: &mut Object) {
         d.insert(s("src"), self.src.to_json());
         d.insert(s("dst"), self.dst.to_json());
         d.insert(s("leader"), self.leader.to_json());
         d.insert(s("mid"), self.mid.to_json());
     }
+
+    fn from_obj(obj: &Object) -> BaseMsg {
+        BaseMsg {
+            src: get!(obj -> "src"; NodeId::from_json),
+            dst: get!(obj -> "dst"; NodeId::from_json),
+            leader: get!(obj -> "leader"; NodeId::from_json),
+            mid: get!(obj -> "mid"; Json::as_string).to_owned()
+        }
+    }
 }
 
+#[derive(PartialEq, Debug)]
 struct InternalMsg {
-    term: usize, 
-    last_entry: usize, 
-    last_term: usize, 
+    term: u64, 
+    last_entry: u64, 
+    last_term: u64, 
 }
 
 impl InternalMsg {
-    fn fill(&self, d: &mut BTreeMap<String, Json>) {
+    fn fill(&self, d: &mut Object) {
         d.add_json("term", self.term);
         d.add_json("last_entry", self.last_entry);
         d.add_json("last_term", self.last_term);
     }
+
+    fn from_json(obj: &Object) -> InternalMsg {
+        
+        InternalMsg {
+            term: get!(obj -> "term"; Json::as_u64),
+            last_entry: get!(obj -> "last_entry"; Json::as_u64),
+            last_term: get!(obj -> "last_term"; Json::as_u64),
+        }
+    }
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub struct Entry {
+    pub key: String,
+    pub value: String,
+}
+
+impl Entry {
+    fn new(key: &str, val: &str) -> Entry {
+        Entry {
+            key: key.to_owned(),
+            value: val.to_owned(),
+        }
+    }
+
+    fn from_json(entry: &Json) -> Entry {
+        let entry = entry
+            .as_object()
+            .expect("entries must be constructed from a json object");
+        Entry {
+            key: get!(entry -> "key"; Json::as_string).to_owned(),
+            value: get!(entry -> "value"; Json::as_string).to_owned(),
+        }
+    }
+}
+
+impl ToJson for Entry {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.add_json("key", self.key.to_owned());
+        d.add_json("value", self.value.to_owned());
+        Json::Object(d)
+    }
+}
+
+#[derive(PartialEq, Debug)]
 enum MsgType {
     OK,
     Fail,
@@ -52,15 +113,15 @@ enum MsgType {
     Put(String, String),
     AppendEntries { 
         details: InternalMsg,
-        entries: Option<Vec<String>> },
+        entries: Option<Vec<Entry>> },
     RequestVote {
         details: InternalMsg,
-        candidate_id: usize,
+        candidate_id: u64,
     },
 }
 
 impl MsgType {
-    fn fill(&self, d: &mut BTreeMap<String, Json>) {
+    fn fill(&self, d: &mut Object) {
         d.add_json("type", self.name().to_owned());
         match *self {
             MsgType::OK | MsgType::Fail | MsgType::Redirect => return,
@@ -91,11 +152,63 @@ impl MsgType {
             MsgType::RequestVote{ .. } => "request_vote",
         }
     }
-}
+    
+    fn from_obj(obj: &Object) -> MsgType {
+        match get!(obj -> "type"; Json::as_string) {
+            "ok" => MsgType::OK,
+            "fail" => MsgType::Fail,
+            "redirect" => MsgType::Redirect,
+            "get" => MsgType::Get(get!(obj -> "key"; Json::as_string).to_owned()),
+            "put" => MsgType::Put(get!(obj -> "key"; Json::as_string).to_owned(),
+                                  get!(obj -> "value";Json::as_string).to_owned()),
+            "append_entries" => MsgType::parse_append_entries(obj),
+            "request_vote" => MsgType::parse_request_vote(obj),
+            _              => unreachable!("unknown message type"),
+        }
+    }
 
+    fn parse_append_entries(obj: &Object) -> MsgType {
+        let int_msg = InternalMsg::from_json(obj);
+        if let Some(entries) = obj.get("entries") {
+            let entries = entries.as_array()
+                .expect("entries must be an array")
+                .iter()
+                .map(|entry| Entry::from_json(entry))
+                .collect();
+
+            MsgType::AppendEntries{details: int_msg, entries: Some(entries)}
+        } else {
+            MsgType::AppendEntries{details: int_msg, entries: None}
+        }
+    }
+
+    fn parse_request_vote(obj: &Object) -> MsgType {
+        let int_msg = InternalMsg::from_json(obj);
+        MsgType::RequestVote{
+            details: int_msg, 
+            candidate_id: get!(obj -> "candidate_id"; Json::as_u64)
+        }
+    }
+}
+#[derive(PartialEq, Debug)]
 pub struct Msg {
     base: BaseMsg,
     msg: MsgType,
+}
+
+impl Msg {
+
+    fn from_str(s: &str) -> Msg {
+        let raw_json = Json::from_str(s)
+            .ok()
+            .expect("parsing json from str failed");
+        let raw = raw_json.as_object()
+            .expect("raw json was not object");
+        let base = BaseMsg::from_obj(raw);
+        let msg = MsgType::from_obj(raw);
+
+        Msg { base: base, msg: msg }
+    }
 }
 
 impl ToJson for Msg {
@@ -130,7 +243,7 @@ fn test_internal_msg_serialize() {
     let details = InternalMsg{ term: 4, last_term: 3, last_entry: 213 };
     let append = MsgType::AppendEntries {
         details: details, 
-        entries: Some(vec![s("x: 13"), s("y: 27")]),
+        entries: Some(vec![Entry::new("x", "13"), Entry::new("y","27")]),
     };
 
     let base = BaseMsg {
@@ -140,5 +253,36 @@ fn test_internal_msg_serialize() {
         mid: s("BABADOOK")
     };
     let msg = Msg { base: base, msg: append};
-    assert_eq!(msg.to_json().to_string(), s("{\"dst\":\"001E\",\"entries\":[\"x: 13\",\"y: 27\"],\"last_entry\":213,\"last_term\":3,\"leader\":\"AA43\",\"mid\":\"BABADOOK\",\"src\":\"13AE\",\"term\":4,\"type\":\"append_entries\"}"));
+    assert_eq!(msg.to_json().to_string(), s("{\"dst\":\"001E\",\"entries\":[{\"key\":\"x\",\"value\":\"13\"},{\"key\":\"y\",\"value\":\"27\"}],\"last_entry\":213,\"last_term\":3,\"leader\":\"AA43\",\"mid\":\"BABADOOK\",\"src\":\"13AE\",\"term\":4,\"type\":\"append_entries\"}"));
+}
+
+#[test]
+fn test_msg_deserialize() {
+    let msg = "{\"dst\":\"001E\",\"leader\":\"AA43\",\"mid\":\"BABADOOK\",\"src\":\"13AE\",\"type\":\"ok\"}";
+    let base = BaseMsg {
+        src: NodeId(['1' as u8, '3' as u8, 'A' as u8, 'E' as u8]), 
+        dst: NodeId(['0' as u8, '0' as u8, '1' as u8, 'E' as u8]),
+        leader: NodeId(['A' as u8, 'A' as u8, '4' as u8, '3' as u8]),
+        mid: s("BABADOOK")
+    };
+    let msg_type = MsgType::OK;
+    assert_eq!(Msg {base: base, msg: msg_type}, Msg::from_str(msg));
+}
+
+#[test]
+fn test_int_msg_deserialize() {
+    let details = InternalMsg{ term: 4, last_term: 3, last_entry: 213 };
+    let append = MsgType::AppendEntries {
+        details: details, 
+        entries: Some(vec![Entry::new("x", "13"), Entry::new("y","27")]),
+    };
+
+    let base = BaseMsg {
+        src: NodeId(['1' as u8, '3' as u8, 'A' as u8, 'E' as u8]), 
+        dst: NodeId(['0' as u8, '0' as u8, '1' as u8, 'E' as u8]),
+        leader: NodeId(['A' as u8, 'A' as u8, '4' as u8, '3' as u8]),
+        mid: s("BABADOOK")
+    };
+    let msg = Msg { base: base, msg: append};
+    assert_eq!(msg, Msg::from_str("{\"dst\":\"001E\",\"entries\":[{\"key\":\"x\",\"value\":\"13\"},{\"key\":\"y\",\"value\":\"27\"}],\"last_entry\":213,\"last_term\":3,\"leader\":\"AA43\",\"mid\":\"BABADOOK\",\"src\":\"13AE\",\"term\":4,\"type\":\"append_entries\"}"));
 }
--- raft/src/node.rs
+++ raft/src/node.rs
@@ -1,16 +1,26 @@
+use std::collections::HashMap;
 use std::str::from_utf8;
 
 use unix_socket::UnixStream;
 use rustc_serialize::json::{Json, ToJson};
 
+#[derive(PartialEq, Debug)]
 pub struct NodeId(pub [u8; 4]);
+
+impl NodeId {
+    pub fn from_json(json: &Json) -> Option<NodeId> {
+        match json.as_string().expect("node id must be string").as_bytes() {
+            [a, b, c, d] => Some(NodeId([a as u8, b as u8, c as u8, d as u8])),
+            _            => None,
+        }
+    }
+}
 
 impl ToJson for NodeId {
     fn to_json(&self) -> Json {
         from_utf8(&self.0).unwrap().to_json()
     }
 }
-
 
 struct BaseNode {
     current_term: usize,
@@ -20,6 +30,7 @@ struct BaseNode {
     last_applied: usize,
     neighbors: Vec<NodeId>,
     port: UnixStream,
+    state_machine: HashMap<String, String>,
 }
 
 enum NodeType {
